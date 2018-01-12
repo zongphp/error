16434d9043e371270aa9cebc04fb4c4f1c4b2ba3
zmingzhi <zmingzhi@yeah.net>
2018-01-12 09:14:39 +0800

    2.0 version

---
+++ src/ErrorFacade.php
@@ -0,0 +1,12 @@
+<?php
+namespace zongphp\error;
+
+use zongphp\framework\build\Facade;
+
+class ErrorFacade extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'Error';
+    }
+}
